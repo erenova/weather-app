43f166e7d8531043b703e88145ac5c495c54ebe7
erenova <erenova6@gmail.com>
2024-02-20 11:17:26 +0300

    add font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      virgil: ["virgil", "sans-serif"],
+    },
+    extend: {
+      screens: {
+        xs: "355px",
+      },
+    },
   },
   plugins: [],
 };
